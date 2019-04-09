727d08792c6182f4057f6ce315178207d00ba708
unknown <clearhuan@qq.com>
2019-04-09 14:52:10 +0800

    change

--- swiper/swiper/settings.py
+++ swiper/swiper/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     # 'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    # 'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
 ]
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- swiper/user/api.py
+++ swiper/user/api.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from lib.sms import send_sms
 
 # Create your views here.
+def submit(request):
+    '''获取短信验证码'''
+    if not request.method == 'POST':
+        return HttpResponse('request method error')
+    phone = request.POST.get('phone')
+    result,msg = send_sms(phone)
+    print(msg)
+    return HttpResponse('send success')
+
+def submit(request):
+    '''通过验证码登录注册'''
+    pass
+def submit(request):
+    '''获取个人资料'''
+    pass
+def submit(request):
+    '''修改个人资料'''
+    pass
+def submit(request):
+    '''头像上传'''
+    pass
